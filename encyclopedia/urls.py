08cb6cf53be9a795e4cb3b34b235127ec2f18fe3
Marce <marceross@gmail.com>
2023-10-17 12:11:49 -0300

    Conditional for showing entry OR no result message

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     # path("marce", views.marce, name="marce"),
     #path("wiki/<str:name>", views.wiki, name="wiki"),
     path("wiki/<str:title>", views.entry_page, name="entry_page"),
+    path("entry", views.index, name="entry"),
     # the directory where the file is configured in views.py
 ]
 
